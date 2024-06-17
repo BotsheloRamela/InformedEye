2c8e3491810e61f0329c39e4ab9fbde099c07811
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-06-17 09:32:00 +0200

    Update Theme

--- app/src/main/java/com/botsheloramela/informedeye/ui/theme/Theme.kt
+++ app/src/main/java/com/botsheloramela/informedeye/ui/theme/Theme.kt
@@ -1,13 +1,17 @@
 package com.botsheloramela.informedeye.ui.theme
 
 import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -91,12 +95,16 @@ private val darkScheme = darkColorScheme(
 @Composable
 fun InformedEyeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = if (darkTheme) {
-        darkScheme
-    } else {
-        lightScheme
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> darkScheme
+        else -> lightScheme
     }
 
     val view = LocalView.current
