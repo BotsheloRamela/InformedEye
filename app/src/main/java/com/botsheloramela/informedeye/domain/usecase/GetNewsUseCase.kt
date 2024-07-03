12d00f0a67ad8f522f94b31f9e79ea9bb63934d1
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-07-03 21:27:13 +0200

    Filter out articles with timestamp titles

--- app/src/main/java/com/botsheloramela/informedeye/domain/usecase/GetNewsUseCase.kt
+++ app/src/main/java/com/botsheloramela/informedeye/domain/usecase/GetNewsUseCase.kt
@@ -1,9 +1,12 @@
 package com.botsheloramela.informedeye.domain.usecase
 
 import androidx.paging.PagingData
+import androidx.paging.filter
 import com.botsheloramela.informedeye.domain.model.Article
 import com.botsheloramela.informedeye.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.util.regex.Pattern
 
 /**
  * Use case class for fetching news articles.
@@ -23,6 +26,25 @@ class GetNewsUseCase(
      */
     operator fun invoke(sources: List<String>): Flow<PagingData<Article>> {
         // Delegate the call to the repository to handle data retrieval
-        return newsRepository.getNews(sources = sources)
+        return newsRepository.getNews(sources = sources).map { pagingData ->
+            pagingData.filter { article ->
+                !isTimestamp(article.title)
+            }
+        }
+    }
+
+    /**
+     * Checks if the provided string is a timestamp.
+     *
+     * @param title The string to check.
+     * @return True if the string is a timestamp, false otherwise.
+     */
+    private fun isTimestamp(title: String): Boolean {
+        val timestampPattern = Pattern.compile(
+            // Date patterns: yyyy-MM-dd, MM/dd/yyyy, dd/MM/yyyy
+            "^\\d{4}[-/]\\d{2}[-/]\\d{2}( \\d{2}:\\d{2}(?::\\d{2})?)?( [A-Z]{2,4})?$|" + // yyyy-MM-dd or yyyy/MM/dd with optional time and timezone
+                    "^\\d{2}[-/]\\d{2}[-/]\\d{4}( \\d{2}:\\d{2}(?::\\d{2})?)?( [A-Z]{2,4})?$"     // dd/MM/yyyy or MM/dd/yyyy with optional time and timezone
+        )
+        return timestampPattern.matcher(title).matches()
     }
 }
