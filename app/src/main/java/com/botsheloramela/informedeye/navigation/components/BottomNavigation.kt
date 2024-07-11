063f34bf3948eca1761e9508e56bccc58724187d
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-07-11 18:07:17 +0200

    Create bottom nav bar

---
+++ app/src/main/java/com/botsheloramela/informedeye/navigation/components/BottomNavigation.kt
@@ -0,0 +1,86 @@
+package com.botsheloramela.informedeye.navigation.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults.IconSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.botsheloramela.informedeye.R
+import com.botsheloramela.informedeye.presentation.Dimensions.ExtraSmallPadding2
+import com.botsheloramela.informedeye.ui.theme.InformedEyeTheme
+
+@Composable
+fun BottomNavigation(
+    items: List<BottomNavItem>,
+    onItemClick: (route: String) -> Unit,
+    selectedItem: Int
+) {
+    NavigationBar(
+        modifier = Modifier.fillMaxWidth(),
+        containerColor = MaterialTheme.colorScheme.surface,
+        tonalElevation = 10.dp
+    ) {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = index == selectedItem,
+                onClick = {onItemClick(item.route)},
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        Icon(
+                            painter = painterResource(id = item.icon),
+                            contentDescription = null,
+                            modifier = Modifier.size(IconSize)
+                        )
+                        Spacer(modifier = Modifier.height(ExtraSmallPadding2))
+                        Text(text = item.title, style = MaterialTheme.typography.labelSmall)
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.secondary,
+                    unselectedTextColor = MaterialTheme.colorScheme.secondary
+                )
+
+            )
+        }
+    }
+}
+
+data class BottomNavItem(
+    val route: String,
+    @DrawableRes val icon: Int,
+    val title: String
+)
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun BottomNavigationPreview() {
+    InformedEyeTheme {
+        BottomNavigation(
+            items = listOf(
+                BottomNavItem("home", R.drawable.ic_home, "Home"),
+                BottomNavItem("search", R.drawable.ic_search, "Search"),
+                BottomNavItem("bookmarks", R.drawable.ic_bookmark, "Bookmarks")
+            ),
+            onItemClick = {},
+            selectedItem = 0
+        )
+    }
+}
